e689b11ffcc1eb40b5238e314f4e4f224113b50e
Adrian Kast <adrian.kast@msg-gillardon.de>
2022-05-03 18:50:08 +0200

    npm lock v2 start working on posts page

---
+++ pages/posts.js
@@ -0,0 +1,50 @@
+import Head from "next/head";
+import Container from "../components/container";
+import HeroPost from "../components/hero-post";
+import Intro from "../components/intro";
+import Layout from "../components/layout";
+import MoreStories from "../components/more-stories";
+import { getAllPosts } from "../lib/api";
+
+export default function Index({ allPosts }) {
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Blog Posts</title>
+        </Head>
+        <Container>
+          <Intro />
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.coverImage}
+              date={heroPost.date}
+              author={heroPost.author}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
+            />
+          )}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        </Container>
+      </Layout>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const allPosts = getAllPosts([
+    "title",
+    "date",
+    "slug",
+    "author",
+    "coverImage",
+    "excerpt",
+  ]);
+
+  return {
+    props: { allPosts },
+  };
+}
