567a870ded4516d9b9de3696a98ab210788605dc
Adrian Kast <55034043+adriankast@users.noreply.github.com>
2023-03-05 10:20:24 +0100

    fix blog footer responsiveness again

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,13 +4,13 @@ import SocialConnect from './social-connect'
 
 export default function Footer() {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2 w-screen">
+    <footer className="bg-accent-1 border-t border-accent-2 w-screen md:w-full">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-        <div className="text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+        <div className="py-28 flex flex-col lg:flex-row items-center mr-4 ml-4 lg:mx-auto lg:my-auto lg:w-1/2">
+        <div className="text-center lg:text-left mb-10 lg:mb-0 lg:pr-4">
           <SocialConnect />
         </div>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4">
             <span
               className="mx-3 font-bold hover:underline"
             ><Link href="/imprint">Imprint</Link>
