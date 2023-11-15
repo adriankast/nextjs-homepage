e55c38ccc665ea281f0d2f4a48114218419ff83c
Adrian Kast <55034043+adriankast@users.noreply.github.com>
2023-11-15 21:25:29 +0100

    make unzip script more useful

--- scripts/unpackNewBlogPost.js
+++ scripts/unpackNewBlogPost.js
@@ -1,19 +1,33 @@
 const decompress = require("decompress");
 const fs = require("fs")
+const blogAssetDir = "./public/assets/blog"
 
 const zips = fs.readdirSync("./newPost");
-console.log(zips);
 
 zips.forEach(zipName => {
     const zipLocation = `./newPost/${zipName}`
 
 decompress(zipLocation, "./newPost/")
   .then((files) => {
-    console.log(files);
+    // console.log(files);
     const [markdown] = files.filter(file => file.path.endsWith(".md"));
+
     const markdownLocation = `./newPost/${markdown.path}`
-    
-    fs.copyFileSync(markdownLocation,`./_posts/${markdown.path}`);
+    const unzipFolder = markdownLocation.slice(0, -3)
+    const newFileName = markdown.path.split(" ").slice(0, -1).join("-").replace(/[^\w\-]/gi,"").toLowerCase()
+    fs.copyFileSync(markdownLocation,`./_posts/${newFileName}.md`);
+
+    const imageFiles = fs.readdirSync(unzipFolder).filter(fname => (fname.endsWith(".svg") || fname.endsWith(".png") || fname.endsWith(".jpeg")))
+    const targetDir = `${blogAssetDir}/${newFileName}`
+    fs.mkdirSync(targetDir)
+    imageFiles.forEach(fname => {
+      const fpath = `${unzipFolder}/${fname}`;
+      fs.copyFileSync(fpath, `${targetDir}/${fname}`)
+      fs.rmSync(fpath)
+    })
+
+
+    fs.rmdirSync(unzipFolder)
     fs.rmSync(markdownLocation)
     fs.rmSync(zipLocation)
   })
