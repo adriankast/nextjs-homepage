9da80570fb27fb6c3e09f4f16e410fa75e835818
Adrian Kast <55034043+adriankast@users.noreply.github.com>
2024-04-01 15:30:48 +0200

    add small welcome animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,30 @@
 import Head from "next/head";
 import News from "../components/news"
 import { PRIMARY_COLOR } from "../lib/constants"
+import { useEffect, useRef } from "react"
 
-export default function Index({ allPosts }) {
+const removeCssProps = (ref) => {
+  if(ref.current) {
+      ref.current.classList.remove("opacity-0");
+      ref.current.classList.remove("-ml-96");
+  }
+}
+
+export default function Index() {
+  const contentBoxRef = useRef(null);
+  useEffect(() => {
+    setTimeout(() => {
+      removeCssProps(contentBoxRef)
+    }, 100);
+  }, [])
+
   return (
     <>
       <Head>
         <title>Welcome 👋</title>
       </Head>
       <div className="grid justify-center h-full items-center">
-        <div className="border-2 font-extrabold font-sans  m-4 border-black">
+        <div ref={contentBoxRef} className={`border-2 font-extrabold font-sans  m-4 border-black -ml-96 opacity-0 `} style={{transition: "ease-in-out 2s opacity,ease-in-out 2s margin-left"}}>
           <div
             className="border-4 border-black"
             style={{ borderColor: PRIMARY_COLOR }}
