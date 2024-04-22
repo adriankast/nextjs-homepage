6726e03439d0c86a9552338b135b12af1dbd03ff
Adrian Kast <55034043+adriankast@users.noreply.github.com>
2024-04-22 08:11:05 +0200

    add workation blog post improve preprocessing

--- scripts/unpackNewBlogPost.js
+++ scripts/unpackNewBlogPost.js
@@ -17,9 +17,11 @@ zips.forEach( zipName =>
       const markdownLocation = `./newPost/${ markdown.path }`
       const unzipFolder = markdownLocation.slice( 0, -3 )
       const newFileName = markdown.path.split( " " ).slice( 0, -1 ).join( "-" ).replace( /[^\w\-]/gi, "" ).toLowerCase()
-      fs.copyFileSync( markdownLocation, `./_posts/${ newFileName }.md` );
+      const newFilePath = `./_posts/${ newFileName }.md`
+      fs.copyFileSync( markdownLocation, newFilePath );
+      removeMarkdownTitle(newFilePath)
 
-      const imageFiles = fs.readdirSync( unzipFolder ).filter( fname => ( fname.endsWith( ".svg" ) || fname.endsWith( ".png" ) || fname.endsWith( ".jpeg" ) ) )
+      const imageFiles = fs.readdirSync( unzipFolder ).filter( fname => ( fname.endsWith( ".svg" ) || fname.endsWith( ".png" ) || fname.endsWith( ".jpeg" ) || fname.endsWith( ".jpg" ) ) )
       const targetDir = `${ blogAssetDir }/${ newFileName }`
       fs.mkdirSync( targetDir )
       imageFiles.forEach( fname =>
@@ -39,3 +41,17 @@ zips.forEach( zipName =>
       console.log( error );
     } );
 } )
+
+function removeMarkdownTitle(filename) {
+
+  post = fs.readFileSync(filename);
+  postByLine = post.toString().split("\n")
+
+  firstSeparator = postByLine.findIndex(line => line === "---");
+  if (firstSeparator === -1) {
+      throw new Error("the meta information is missing in the post")
+  }
+  cutPost = postByLine.slice(firstSeparator);
+
+  fs.writeFileSync(filename, cutPost.join("\n"))
+}
